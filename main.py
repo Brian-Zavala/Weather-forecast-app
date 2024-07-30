a20c4840fa5afda4f74b87be2fc96c70cb22be79
Quadfather <quadfather22@hotmail.com>
2024-07-29 21:04:06 -0500

    Added all widgets.

--- main.py
+++ main.py
@@ -3,10 +3,13 @@ import streamlit as st
 
 st.set_page_config(layout="wide")
 
-st.header("Weather Forecast")
+st.title("Weather Forecast")
 
-place = st.text_input("Place:")
+place = st.text_input("Location:")
 
-days = st.slider("Forecast Days", 1, 7)
+days = st.slider("Forecast Days", 1, 5, help="Select the day you'd like to see")
 
-data = st.selectbox("Select data to view", options='')
+data = st.selectbox("Select data to view",
+                   ("Temperature", "sky"))
+
+st.subheader(f"Temperature for the next {days} days in {place}")
