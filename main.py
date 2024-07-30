e89684f66cd0dc05d5ea1ab26306459c53c89c5a
Quadfather <quadfather22@hotmail.com>
2024-07-29 21:23:25 -0500

    added plot with temps and days.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import plotly.express as px
 
 
 st.set_page_config(layout="wide")
@@ -13,3 +14,10 @@ data = st.selectbox("Select data to view",
                    ("Temperature", "sky"))
 
 st.subheader(f"Temperature for the next {days} days in {place}")
+
+dates = ["2022-25-10", "2022-26-10", "2022-27-10", "2022-28-10", "2022-29-10",]
+temperature = ["100", "99", "87", "96", "107"]
+temperature = [days * i for i in temperature]
+
+figure = px.line(x=dates, y=temperature, labels={"x": "Date", "y": "Temperature"},)
+st.plotly_chart(figure, use_container_width=True)
